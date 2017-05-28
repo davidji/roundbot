0d7f5cd018fea3e3b693ed52d4d1565805b1ce9b
David Ireland <david@lshift.net>
2017-05-28 15:44:19 +0100

    work in progress: head segment

--- hardware/raspberrypi.py
+++ hardware/raspberrypi.py
@@ -9,4 +9,10 @@ class aplus:
 
     @staticmethod
     def cut_holes():
-        return corners([58,49], fixings.M2_5.cut())
+        return corners(58,49)(fixings.M2_5.cut())
+
+class zero:
+    d = [65.0, 30.0]
+    h = [58.0,23.0]
+    holes = corners(*h)
+    fixing = fixings.M2_5
