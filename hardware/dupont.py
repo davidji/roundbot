5ecb01f457f5918e0838ba35145c386c54592086
David Ireland <david@lshift.net>
2017-07-26 22:11:44 +0100

    holder for modules with headers

---
+++ hardware/dupont.py
@@ -0,0 +1,55 @@
+from solid import *
+from solid.utils import *
+
+import util
+from util import inch_to_mm
+from fixings import M2
+
+TENTH=inch_to_mm(0.1)
+def tenths(x):
+    return inch_to_mm(0.1*x)
+
+HEIGHT=12
+WIRE_THICKNESS=1.5
+BASE_THICKNESS=2.0
+TOLERANCE=0.25
+
+def single_in_line(n):
+    return (
+        linear_extrude(HEIGHT)(
+            square([tenths(n+1)+2.0+TOLERANCE, tenths(1)+2.0+TOLERANCE], center=True)) +
+        hole()(up(BASE_THICKNESS)(linear_extrude(HEIGHT)(square([tenths(n+1)+TOLERANCE, tenths(1)+TOLERANCE], center=True))) +
+               linear_extrude(BASE_THICKNESS)(
+                   square([tenths(n)+WIRE_THICKNESS, WIRE_THICKNESS], center=True) +
+                   left(tenths(float(n)/2))(square([tenths(1)+TOLERANCE, tenths(1)+TOLERANCE], center=True)))))
+
+def dual_in_line(spacing, n):
+    single = left(spacing/2)(rotate([0,0,90])(single_in_line(n)))
+    return (single + 
+            mirror([1,0,0])(single) + 
+            linear_extrude(BASE_THICKNESS)(
+                square([spacing+tenths(1)+2.0+TOLERANCE, tenths(n+1)+2.0+TOLERANCE], center=True) -
+                union()(*(translate(x)(M2.cut()) for x in util.corners(spacing-tenths(1)-6.0, tenths(n+1)-2.0)))))
+
+def spacer(n):
+    return (
+        cube([tenths(n), BASE_THICKNESS, tenths(1)+1.0]) +
+        cube([tenths(n), HEIGHT, tenths(1)]) +
+        hole()(up(tenths(0.5) - 0.5)(union()(*(
+            right(tenths(x+0.5)-0.5)(cube([1.0, HEIGHT, tenths(0.5) + 0.5 + BASE_THICKNESS]))
+            for x in range(n))))))
+
+def spacer_collection():
+    return (
+        union()(*(translate([x*5, 0])(spacer(1)) for x in range(16))) +
+        union()(*(translate([x*10, 15])(spacer(2)) for x in range(8))) +
+        union()(*(translate([x*20, 30])(spacer(4)) for x in range(4))) +
+        union()(*(translate([x*40, 45])(spacer(8)) for x in range(2))))
+
+def export_scad():
+    util.save('dupont-15', single_in_line(15))
+    util.save('nucleo32', dual_in_line(inch_to_mm(0.6), 15))
+    util.save('dupont-spacers', spacer_collection())
+
+if __name__ == '__main__':
+    export_scad()
